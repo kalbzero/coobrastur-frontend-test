851cbcb2ec95eeda85a1d6226a6b5075761a1303
Edward Ramos <edward.ramos.guimaraes@gmail.comm>
2021-03-05 18:30:48 -0300

    feat: create components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { UserComponent } from './componets/user/user.component';
+import { UserListComponent } from './componets/user/user-list/user-list.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: UserComponent
+  },
+  {
+    path: 'add',
+    component: UserListComponent
+  },
+  {
+    path: 'edit/:id',
+    component: UserListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
